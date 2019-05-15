98febc73ef44c61483ab8c2c93c534dbe33aa80c
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-15 16:59:52 +0100

    finished 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,13 +4,17 @@ describe DockingStation do
 
 	let(:bike) {double(:bike)}
 	let(:station) {DockingStation.new}
+	
 
-  it "can dock a bike" do
+	it "can dock a bike" do
+		allow(bike).to receive(:broken=)
+
     expect(station.dock(bike)).to include(bike)
 	end
 
 
 	it "can show a docked bike" do
+		allow(bike).to receive(:broken=)
 
 		station.dock(bike)
 		
@@ -22,11 +26,13 @@ describe DockingStation do
   end
 
 	it 'throws an error if bike is already docked' do
+		allow(bike).to receive(:broken=)
 		DockingStation::DEFAULT_CAPACITY.times{station.dock(bike)}
 		expect{station.dock(bike)}.to raise_error("docking station is full")
 	end
 
 	it 'can set a default capacity or specified capacity' do
+		allow(bike).to receive(:broken=)
 		station_25 = DockingStation.new(25)
 
 		25.times{ station_25.dock(bike) }
@@ -34,7 +40,7 @@ describe DockingStation do
 	end 
 
 	it 'can accept a broken bike' do
-
+		allow(bike).to receive(:broken=)
 		station.dock(bike,true)
 		
 		expect(station.bikes).to include(bike)
@@ -42,7 +48,8 @@ describe DockingStation do
 	end
 
 	it 'does not release broken bikes' do
-	
+		allow(bike).to receive(:broken=)
+		allow(bike).to receive(:broken).and_return(true)
 		station.dock(bike, true)
 
 		expect{station.release_bike}.to raise_error("no working bikes")
