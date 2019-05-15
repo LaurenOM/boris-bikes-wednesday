d29a4de81adffa8e664ed79c111fb3480050c505
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-15 15:56:12 +0100

    finished step 16

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,6 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-#  it "responds to release bike method" do
-# 	docking_station = DockingStation.new
-# 	docking_station.release_bike
-
-# 	expect(docking_station).to respond_to(:release_bike)
-
-#  end
 
 	it "gets a bike and expects it to be working" do
 		bike = Bike.new
@@ -15,21 +8,22 @@ describe DockingStation do
 		expect(bike.working?).to eq(true)
 	end
 
-  # it "can dock a bike" do
-	# 	station = DockingStation.new
+  it "can dock a bike" do
+		station = DockingStation.new
+		bike = Bike.new
 
-  #   expect(station.dock(nil)).to eq(bike)
-	# end
+    expect(station.dock(bike)).to include(bike)
+	end
 
 
-	# it "can show a docked bike" do
-	# 	docking_station = DockingStation.new
-	# 	bike = Bike.new
+	it "can show a docked bike" do
+		docking_station = DockingStation.new
+		bike = Bike.new
 
-	# 	docking_station.dock(bike)
+		docking_station.dock(bike)
 		
-	# 	expect(docking_station.bike).to eq(bike)
-  # end
+		expect(docking_station.bikes).to include(bike)
+  end
   
   it 'can throw error if no bike is docked' do
     station = DockingStation.new
@@ -40,7 +34,8 @@ describe DockingStation do
 	it 'throws an error if bike is already docked' do
 		station = DockingStation.new
 		bike = Bike.new
-		20.times{station.dock(bike)}
+
+		DockingStation::DEFAULT_CAPACITY.times{station.dock(bike)}
 		expect{station.dock(bike)}.to raise_error("docking station is full")
 	end
 end
